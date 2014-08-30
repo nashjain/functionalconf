8d64d3ad64ab03e12281bff9296a987ee9be051b
Naresh Jain <nashjain@gmail.com>
2014-08-31 01:08:40 +0530

    updated the deploy script to ignore dev files and only deploy the output html files

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -zrpt --delete --progress --force --delete-excluded --exclude=deploy.sh --exclude=.gitignore --exclude=.DS_Store --exclude=.git --exclude=.idea ~/Sites/functionalconf/ naresh@naresh.webfactional.com:/home/naresh/webapps/functionalconf/
+rsync -zrpt --delete --progress --force --delete-excluded --exclude=deploy.sh --exclude=.gitignore --exclude=.DS_Store --exclude=.git --exclude=.gitkeep --exclude=.idea --exclude=scss --exclude=templates --exclude=compile.py --exclude=config.rb ~/Sites/functionalconf/ naresh@naresh.webfactional.com:/home/naresh/webapps/functionalconf/
