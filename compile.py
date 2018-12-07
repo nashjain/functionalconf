6fd0ed0f321233cdaeabff8ca9949bbbab3daae8
Naresh Jain <nashjain@gmail.com>
2018-12-08 01:48:01 +0530

    know your speakers

--- compile.py
+++ compile.py
@@ -5,7 +5,7 @@ from jinja2 import Environment, FileSystemLoader
 PUBLISH_ROOT = '.'
 JINJA_ENV = Environment(loader=FileSystemLoader('templates'))
 
-templates = ["index.html", "about.html", "register.html", "schedule.html", "archive.html", "speaker.html", "sponsor.html", "venue.html", "profile.html", "proposal.html", "proposals.html", "speakers.html", "sponsors.html", "sponsor-guide.html", "2017/index.html", "2017/schedule.html", "2016/index.html", "2016/schedule.html", "2016/sponsor.html", "2015/index.html", "2015/schedule.html", "2015/sponsor.html", "2014/index.html", "2014/schedule.html", "2014/sponsor.html"]
+templates = ["index.html", "about.html", "register.html", "schedule.html", "archive.html", "know-thy-speakers.html", "speaker.html", "sponsor.html", "venue.html", "profile.html", "proposal.html", "proposals.html", "speakers.html", "sponsors.html", "sponsor-guide.html", "2017/index.html", "2017/schedule.html", "2016/index.html", "2016/schedule.html", "2016/sponsor.html", "2015/index.html", "2015/schedule.html", "2015/sponsor.html", "2014/index.html", "2014/schedule.html", "2014/sponsor.html"]
 
 for template_file in templates:
     template = JINJA_ENV.get_template("%s" % template_file)
